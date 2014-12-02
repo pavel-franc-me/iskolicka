8631658278953040a4c919e065aeab0130c9fc78
Hana Francova <hana.francova@gmail.com>
2014-12-02 22:40:22 +0100

    Upraveno zobrazeni detailu

--- iskolicka/app/view/LessonDetail.js
+++ iskolicka/app/view/LessonDetail.js
@@ -1,4 +1,47 @@
 Ext.define('iskolicka.view.LessonDetail', {
+    extend: 'Ext.tab.Panel',
+    xtype: 'lessondetail',
+    config: {
+        fullscreen: true,
+        tabBarPosition: 'bottom',
+
+        items: [
+            {
+                title: 'Detail',
+                iconCls: 'home',
+                html: [
+                    'Detail - texty'
+                ].join("")
+            },
+            {
+                title: 'Dictionary',
+                iconCls: 'bookmarks',
+                html: [
+                    'Slovicka - slovní zásoba'
+                ].join("")
+            },
+            {
+                title: 'Test',
+                iconCls: 'action',
+                html: [
+                    'Testovani - zkoušení slovíček'
+                ].join("")
+            }
+        ]
+    }
+});
+
+
+function getDetail(id) {
+    var result =  $.ajax({
+        url:"script/ajaxScript.php?id="+id,
+        dataType: 'html',
+        async: false
+    }).responseText;
+    return result;
+}
+/*
+Ext.define('iskolicka.view.LessonDetail', {
     extend: 'Ext.Panel',
     xtype: 'lessondetail',
 
@@ -16,27 +59,20 @@ Ext.define('iskolicka.view.LessonDetail', {
         this.setHtml(getDetail(this.getData().firstName));
     }
 });
-
-
-function getDetail(id) {
-    Ext.data.aj
-    var result =  $.ajax({
-        url:"http://mobile.iskolicka.cz/script/ajaxScript.php?id="+id,
-        dataType: 'html',
-        async: false
-    }).responseText;
-    return result;
-}
-/*
-var ajax = Ext.Ajax.request({
-        url: 'ajax_demo/sample.json',
-        success: function(response, opts) {
-            var obj = Ext.decode(response.responseText);
-                console.dir(obj);
-        },
-        failure: function(response, opts) {
-            console.log('server-side failure with status code ' + response.status);
-        }
-    });
 */
+
+
+
+/*
+ var ajax = Ext.Ajax.request({
+ url: 'ajax_demo/sample.json',
+ success: function(response, opts) {
+ var obj = Ext.decode(response.responseText);
+ console.dir(obj);
+ },
+ failure: function(response, opts) {
+ console.log('server-side failure with status code ' + response.status);
+ }
+ });
+ */
 
