c8f72c5de52b629261d3192239369cc92f2a4b71
Pavel Franc <pavel.dataapex@gmail.com>
2014-12-11 21:50:13 +0100

    listeners test

--- iskolicka/app/view/LessonDetail.js
+++ iskolicka/app/view/LessonDetail.js
@@ -1,30 +1,40 @@
 Ext.define('iskolicka.view.LessonDetail', {
-    extend: 'Ext.tab.Panel',
-    xtype: 'lessondetail',
-    config: {
-        fullscreen: true,
-        tabBarPosition: 'bottom',
+	extend: 'Ext.tab.Panel',
+	xtype: 'lessondetail',
+	counter: 0,
+	config: {
+		fullscreen: true,
+		tabBarPosition: 'bottom',
 
-        items: [
-            {
-                title: 'Detail',
-                iconCls: 'home',
-                html: 'Detail - texty'
-            },
-            {
-                title: 'Dictionary',
-                iconCls: 'bookmarks',
-                html: 'Slovicka - slovní zásoba'
-            },
-            {
-                title: 'Test',
-                iconCls: 'action',
-                html: 'Testovani - zkoušení slovíček'
-            }
-        ]
-    }
+		items: [
+			{
+				title: 'Detail',
+				id: 'detail',
+				iconCls: 'home',
+				html: 'Detail - texty',
+				listeners: {
+					show: function() {
+						var cnt = this.getParent().incrementCounter();
+						this.setHtml('Detail - texty ' + cnt);
+					}
+				}
+			},
+			{
+				title: 'Dictionary',
+				iconCls: 'bookmarks',
+				html: 'Slovicka - slovní zásoba'
+			},
+			{
+				title: 'Test',
+				iconCls: 'action',
+				html: 'Testovani - zkoušení slovíček'
+			}
+		]
+	},
+	incrementCounter: function() {
+		this.counter++;
+		return this.counter;
 
-
-
+	}
 });
 
