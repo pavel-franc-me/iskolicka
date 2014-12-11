d54320a85e7007ae493cc08e7690b6345528d7ec
Hana Francova <hana.francova@gmail.com>
2014-12-11 21:51:46 +0100

    Úprava načítání detailu

--- iskolicka/app/view/LessonDetail.js
+++ iskolicka/app/view/LessonDetail.js
@@ -1,6 +1,7 @@
 Ext.define('iskolicka.view.LessonDetail', {
     extend: 'Ext.tab.Panel',
     xtype: 'lessondetail',
+    text1: 'blabla',
     config: {
         fullscreen: true,
         tabBarPosition: 'bottom',
@@ -9,22 +10,46 @@ Ext.define('iskolicka.view.LessonDetail', {
             {
                 title: 'Detail',
                 iconCls: 'home',
-                html: 'Detail - texty'
+                html: 'Detail - texty',
+                badgeText: 'Tip.',
+                initialize: function() {
+                    console.log("console....%o",this)
+                    this.setHtml("stranka 1");
+                }
+
             },
             {
                 title: 'Dictionary',
                 iconCls: 'bookmarks',
-                html: 'Slovicka - slovní zásoba'
+                html: 'Slovicka - slovní zásoba',
+                initialize: function() {
+                    this.setHtml("stranka 2");
+                }
             },
             {
                 title: 'Test',
                 iconCls: 'action',
-                html: 'Testovani - zkoušení slovíček'
+                html: 'Testovani - zkoušení slovíček',
+                initialize: function() {
+                    this.setHtml("stranka 3");
+                }
             }
         ]
+    },
+    detail: function(id) {
+        var result =  $.ajax({
+         url:"http://mobile.iskolicka.cz/script/ajaxScript.php?id="+id,
+         dataType: 'html',
+         async: false
+         }).responseText;
+         return result;
+    },
+    initialize: function() {
+        console.log("parent....%o",this.items.items[0].html)
+        this.items.items[0].setHtml(this.detail(this.config.data["firstName"]));
+
     }
 
 
 
 });
-
