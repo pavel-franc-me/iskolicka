4777808fbef43b8457caee1918019b8c36ca89b4
Pavel Franc <pavel.dataapex@gmail.com>
2014-12-13 23:12:35 +0100

    iphone mockup

--- iskolicka/app/view/LessonDetail.js
+++ iskolicka/app/view/LessonDetail.js
@@ -1,58 +1,65 @@
 Ext.define('iskolicka.view.LessonDetail', {
-    extend: 'Ext.tab.Panel',
-    xtype: 'lessondetail',
+	extend: 'Ext.tab.Panel',
+	xtype: 'lessondetail',
 
-    config: {
-        fullscreen: true,
-        tabBarPosition: 'bottom',
+	config: {
+		fullscreen: true,
+		tabBarPosition: 'bottom',
 
-        items: [
-            {
-                dataLoaded : false,
-                title: 'Detail',
-                iconCls: 'home',
-                html: 'Detail - texty',
-                badgeText: 'Tip.',
-                initialize: function() {
-                    console.log("console....%o",this)
-                    this.setHtml("stranka 1");
-                },
-                listeners: {
-                    show: function() {
-                        if(!this.dataLoaded) {
-                            this.setHtml(this.getParent().getText());
-                            this.dataLoaded = true;
-                        }
-                    }
-                }
-            },
-            {
-                title: 'Dictionary',
-                iconCls: 'bookmarks',
-                html: 'Slovicka - slovní zásoba',
-                initialize: function() {
-                    this.setHtml("stranka 2");
-                }
-            },
-            {
-                title: 'Test',
-                iconCls: 'action',
-                html: 'Testovani - zkoušení slovíček',
-                initialize: function() {
-                    this.setHtml("stranka 3");
-                }
-            }
-        ]
-    },
-    detail: function(id) {
-        var result =  $.ajax({
-         url:"http://mobile.iskolicka.cz/script/ajaxScript.php?id="+id,
-         dataType: 'html',
-         async: false
-         }).responseText;
-         return result;
-    },
-    getText : function() {
-        return this.detail(this.config.data["firstName"]);
-    }
+		items: [
+			{
+				dataLoaded: false,
+				title: 'Detail',
+				iconCls: 'home',
+				html: 'Detail - texty',
+				badgeText: 'Tip.',
+				listeners: {
+					show: function () {
+						this.getParent().foo(this);
+					}
+				}
+			},
+			{
+				title: 'Dictionary',
+				iconCls: 'bookmarks',
+				html: 'Slovicka - slovní zásoba',
+				initialize: function () {
+					this.setHtml("stranka 2");
+				},
+				listeners: {
+					show: function () {
+						this.getParent().foo(this);
+					}
+				}
+			},
+			{
+				title: 'Test',
+				iconCls: 'action',
+				html: 'Testovani - zkoušení slovíček',
+				listeners: {
+					show: function () {
+						this.getParent().foo(this);
+					}
+				}
+			}
+		]
+	},
+	foo: function (page) {
+		if (page.dataLoaded) {
+			return;
+		}
+        page.setHtml(this.getText());
+		page.dataLoaded = true;
+	},
+	detail: function (id) {
+		var result = $.ajax({
+			url: "http://mobile.iskolicka.cz/script/ajaxScript.php?id=" + id,
+			dataType: 'html',
+			async: false
+		}).responseText;
+		return result;
+	},
+	getText: function () {
+		return this.detail(this.config.data["firstName"]);
+	}
 });
